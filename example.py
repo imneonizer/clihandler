0c4e1603c0936b277e603137957e9be818e1f053
Nitin Rai <mneonizer@gmail.com>
2020-04-16 23:45:29 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 import clihandler as cli
 
 
-#p = cli.call(name="pip", cmd="pip install imthread imchrome nitin_raaii")
 p = cli.call(name="ping", cmd="ping -n 3 127.0.0.1")
 
 print("execution started")
